6e0ace1921d458d98d3624e1396d7f39e123007e
ybluesky <xjz1688@163.com>
2015-12-28 16:45:08 +0800

    delete unit

--- unit/config/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * 网站基本配置信息
- */
-return [
-    'basePath'  =>  dirname(__DIR__),
-    'request'   =>  include(__DIR__.'/request.php')
-];
--- unit/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-require(__DIR__ . '/../vendor/autoload.php');
-require(__DIR__ . '/../h2o.php');
-(new H2O\web\Application(include(__DIR__.'/config/web.php')))->run();
-?>
