bfe2253603466c13f0442aaa413acc4876cb517d
ybluesky <xjz1688@163.com>
2015-12-28 16:43:01 +0800

    web controller

--- web/application.php
+++ web/application.php
@@ -30,7 +30,6 @@ class Application extends H2O\base\Application
 	{
 		$request = new Request(isset($this->_config['request'])?$this->_config['request']:[]); //初始请求
 		$dd = $request->getRoute();
-		print_r($dd);
 		$this->runController($dd);
 	}
 }
---
+++ web/controller.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * 所有控制器的基类
+ * @category   H2O
+ * @package    base
+ * @author     Xujinzhang <xjz1688@163.com>
+ * @version    0.1.0
+ */
+namespace H2O\web;
+use H2O;
+class Controller extends H2O\base\Controller
+{
+	
+}
