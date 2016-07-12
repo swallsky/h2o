fa0016b22106fbbb06f72999b26cbed2c14a142e
ybluesky <xjz1688@163.com>
2016-07-12 16:30:02 +0800

    caching memcache

---
+++ caching/MemCache.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * memcache缓存
+ * @category   H2O
+ * @package    caching
+ * @author     Xujinzhang <xjz1688@163.com>
+ * @version    0.1.0
+ */
+namespace H2O\caching;
+use H2O;
+class MemCache
+{
+    
+}
