9ce31572c522d617b0e168d8b23f22157d7e27ac
ybluesky <xjz1688@163.com>
2016-10-18 17:26:41 +0800

    daemon fixed

--- console/Daemon.php
+++ console/Daemon.php
@@ -29,7 +29,7 @@ class Daemon
     {
         $this->_logpath = APP_RUNTIME.DS.'console'.DS; //日志目录
         $this->_phpBin();
-        $this->_daemonson = $this->_phpBin.' '.$GLOBALS['argv'][0].' ';
+        $this->_daemonson = $this->_phpBin.' '.realpath($GLOBALS['argv'][0]).' '; //返回标准备的命令行
     }
     /**
      * 读取当前运行环境的php执行目录
@@ -38,7 +38,7 @@ class Daemon
     {
         $handle = popen('which php', 'r');
         $bin = fread($handle, 2096);
-        $this->_phpBin = empty($bin)?$this->_phpBin:$bin;
+        $this->_phpBin = empty($bin)?$this->_phpBin:trim($bin);
         pclose($handle);
     }
     /**
@@ -60,6 +60,16 @@ class Daemon
         }
     }
     /**
+     * 查看当前任务列表
+     */
+    public function actGetTask()
+    {
+        $tasks = $this->_getTasks();
+        foreach($tasks as $v){
+            echo $v.PHP_EOL;
+        }
+    }
+    /**
      * 根据队列配置启动队列 只支持单个子进程任务
      */
     private function _startProc()
@@ -68,14 +78,13 @@ class Daemon
         $tasks = $this->_getTasks();
         foreach($adaemons as $que)
         {
-            $_logFile = $this->_logpath . 'daemon' . DS . $que['route'] . DS . date('Ymd') . '.log'; //根据路由规则按天记录
-            for($_taskno=1;$_taskno<=$que['daemonum'];$_taskno++)
-            {
-                if(!in_array($que['route'],$tasks)){//每个任务只充许一个进程在执行
-                    $_cmd = "{$this->_daemonson} @service.daemon --c={$que['route']} >>{$_logFile} 2>&1 &";
-                    $_pp = popen($_cmd, 'r');
-                    pclose($_pp);
-                }
+            $tmpdir = $this->_logpath.'daemon'.DS.$que['route']; //当前任务日志目录
+            file::createDirectory($tmpdir); //如果目录不存在,则创建
+            $_logFile = $tmpdir.DS.date('Ymd').'.log'; //根据路由规则按天记录
+            if(!in_array($que['route'],$tasks)){//每个任务只充许一个进程在执行
+                $_cmd = "{$this->_daemonson} @service.daemon --c={$que['route']} >>{$_logFile} 2>&1 &".PHP_EOL;
+                $_pp = popen($_cmd, 'r');
+                pclose($_pp);
             }
         }
     }
@@ -84,8 +93,8 @@ class Daemon
      */
     private function _getTasks()
     {
-        $_cmd = "ps -ef | grep -v 'grep' | grep '{$GLOBALS['argv'][0]}' | awk '{print $11}'\n"; //$GLOBALS['argv'][0]为命令行的执行的php文件名
-        $_pp = @popen($_cmd, 'r');
+        $_cmd = "ps -ef | grep -v 'grep' | grep '{$this->_daemonson} @service.daemon' | awk '{print $13}'\n";
+        $_pp = popen($_cmd, 'r');
         $res = []; //返回所有正在执行的任务名
         if($_pp){//查看命令行是否有结果
             while(!feof($_pp)) {
@@ -95,28 +104,51 @@ class Daemon
                 $res[] = $_line;
             }
         }
-        @pclose($_pp);
+        pclose($_pp);
         return $res;
     }
     /**
-     * 检测自身进程，同时只允许运行一个实例
+     * 检测自身进程，同时只允许运行一个主进程
+     * stat 中的参数意义如下：
+    D 不可中断 Uninterruptible（usually IO）
+    R 正在运行，或在队列中的进程
+    S 处于休眠状态
+    T 停止或被追踪
+    Z 僵尸进程
+    W 进入内存交换（从内核2.6开始无效）
+    X   死掉的进程
+
+    < 高优先级
+    n   低优先级
+    s   包含子进程
+    +   位于后台的进程组
      * @return	NULL
      */
     private function _checkSelfProc()
     {
-        $_cmd = "ps -ef | grep -v 'grep' | grep '{$GLOBALS['argv'][0]}' | awk '{print $10}'\n"; //$GLOBALS['argv'][0]为命令行的执行的php文件名
-        $_procTotal = 0;
-        $_pp = @popen($_cmd, 'r');
+        $_cmd = "ps aux | grep -v 'grep' | grep '{$GLOBALS['argv'][0]} {$GLOBALS['argv'][1]}' | awk '{print $2,$8}'\n"; //查找守护进程执行状态和进程ID
+        $_pp = popen($_cmd, 'r');
+        $_akill = [];//需要清理的进程
+        $_ptotal = 0; //启动的进程数
         if($_pp){//查看命令行是否有结果
             while(!feof($_pp)) {
                 $_line = trim(fgets($_pp));
                 if(empty($_line)) continue;
-                if($_line != $GLOBALS['argv'][1]) continue;//匹配是不是主进程脚本
-                $_procTotal++;
+                list($_pid, $_status) = explode(' ', $_line);
+                if($_status != 'S+'){//不是正在执行的状态,将记录kill
+                    $_akill[] = $_pid;
+                }else{
+                    $_ptotal++; //当前运行进程加1
+                }
             }
         }
-        @pclose($_pp);
-        if($_procTotal>1) exit();
+        if(!empty($_akill)){
+            foreach($_akill as $_kid){ //清理异常进程
+                popen("kill -9 ".$_kid,'r');
+            }
+        }
+        pclose($_pp);
+        if($_ptotal>1) exit(); //当前已有守护进程存在,不需要再启动
         return;
     }
 }
